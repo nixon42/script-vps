8407550a664c6a51a25c230b3757bfc69bffdcea
nixon42 <aliepcahyo@gmail.com>
2025-04-03 02:55:39 +0700

    enable bbr

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@
 apt update && apt upgrade -y
 apt install -y tmux vim curl python3-venv
 
+# enable bbr
+modprobe tcp_bbr
+sysctl net.core.default_qdisc=fq
+sysctl net.ipv4.tcp_congestion_control=bbr
+
 # install sing-box
 ./setup-singbox.sh
 
